181e6f5e199591fdfdff4dfe8410b30906659b93
ado durak <76266634+Alginest@users.noreply.github.com>
2022-04-17 21:38:18 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import { makeStyles } from "@material-ui/core";
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home/Home";
+import CoinPage from "./pages/CoinPage/CoinPage";
+const useStyles = makeStyles(() => ({
+  App: {
+    backgroundColor: "#14161a",
+    color: "white",
+    minHeight: "100vh",
+  },
+}));
+
+function App() {
+  const classes = useStyles();
+
+  return (
+    <BrowserRouter>
+      <div className={classes.App}>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} exact />
+          <Route path="/coins/:id" element={<CoinPage />} exact />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
